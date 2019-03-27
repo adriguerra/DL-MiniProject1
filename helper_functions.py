85d0501a03d69f90e86883b354cd747efcac7740
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-03-27 12:11:26 +0100

    Move model-loop to notebook

--- helper_functions.py
+++ helper_functions.py
@@ -83,30 +83,29 @@ def compute_nb_errors(model, data_input, data_target):
 
     return nb_data_errors
 
-def compute_errors(models, train_input, train_classes, test_input, test_classes, stds):
+def compute_errors(m, train_input, train_classes, test_input, test_classes, stds):
     """Computes the train errors and test errors of the given models
     for the given standard deviations on the train_input, test_input data
     with the labels in the train_classes and test_classes.
     If the standard deviations parameter is None, a Xavier
     initialization is performed."""
 
-    for m in models:
-        model = m()
-        if(stds is None):
+    model = m()
+    if(stds is None):
+        for p in model.parameters():
+            if len(p.size()) == 2:
+                updated_tensor, std = xavier_normal_(p.data.normal_(0, 1), gain=1)
+        print("Computed standard deviation according to 'Xavier initialization': {:.3f}".format(std))
+        stds = [std]
+    for std in stds:
+        if std > 0:
             for p in model.parameters():
-                if len(p.size()) == 2:
-                    updated_tensor, std = xavier_normal_(p.data.normal_(0, 1), gain=1)
-            print("Computed standard deviation according to 'Xavier initialization': {:.3f}".format(std))
-            stds = [std]
-        for std in stds:
-            if std > 0:
-                for p in model.parameters():
-                    p.data.normal_(0, std)
+                p.data.normal_(0, std)
 
-            train_model(model, train_input, train_classes)
-            print('std {:f} {:s} train_error {:.02f}% test_error {:.02f}%'.format(
-                    std,
-                    m.__name__,
-                    compute_nb_errors(model, train_input,
-                                      train_classes) / train_input.size(0) * 100,
-                    compute_nb_errors(model, test_input, test_classes) / test_input.size(0) * 100))
+        train_model(model, train_input, train_classes)
+        print('std {:f} {:s} train_error {:.02f}% test_error {:.02f}%'.format(
+                std,
+                m.__name__,
+                compute_nb_errors(model, train_input,
+                                  train_classes) / train_input.size(0) * 100,
+                compute_nb_errors(model, test_input, test_classes) / test_input.size(0) * 100))
