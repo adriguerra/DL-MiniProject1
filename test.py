d6b4ed2c2567c69759a54e2ddc48924fb865dd52
aelamran <ayyoub.elamrani4@gmail.com>
2019-05-17 14:39:41 +0200

    final modifs

--- test.py
+++ test.py
@@ -1,29 +1,18 @@
 from cnn_with_ws import *
-import numpy as np
 from cnn_without_ws import *
 
 print("-----Convolution Neural Network with Weight sharing-----")
 
 # Convolution Neural Network with weight sharing and auxiliary loss
-#train_with_ws(digit_scalar=1)
+train_with_ws(digit_scalar=1)
 
 # Convolution Neural Network with weight sharing and without auxiliary loss
-#train_with_ws(digit_scalar=0)
+train_with_ws(digit_scalar=0)
 
 print("-----Convolution Neural Network without Weight sharing-----")
 
 # Convolution Neural Network without weight sharing and auxiliary loss
-#train_without_ws(digit_scalar=1)
+train_without_ws(digit_scalar=1)
 
 # Convolution Neural Network without weight sharing and without auxiliary loss
-#train_without_ws(digit_scalar=0)
-
-list_acc_dig_for_std = []
-list_acc_ord_for_std = []
-for i in range(10):
-    a,b = train_without_ws(digit_scalar=1)
-    list_acc_dig_for_std.append(a)
-    list_acc_ord_for_std.append(b)
-
-print("digit pred std :",np.std(list_acc_dig_for_std))
-print("order pred std :",np.std(list_acc_ord_for_std))
+train_without_ws(digit_scalar=0)
